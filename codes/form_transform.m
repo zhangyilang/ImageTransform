13786aede4ebcc27c47c23c67cf31db6de8cd7b1
Guardianzc <710248571@qq.com>
2020-05-02 16:30:34 +0800

    Question2

--- codes/form_transform.m
+++ codes/form_transform.m
@@ -1,6 +1,8 @@
 function [transform] = form_transform(shape_A, shape_swl, origin, map)
 if length(origin) == 4
+    % 形成二维坐标（,3）——(x,y,1)，然后求逆，这里是正方形框，故有四个参数
     for i = 1:2
+        %归一化
         origin(i) = origin(i) - shape_A(1)/2;
         map(i) = map(i) - shape_swl(1)/2;
     end
@@ -8,12 +10,13 @@ if length(origin) == 4
         origin(i) = origin(i) - shape_A(2)/2;
         map(i) = map(i) - shape_swl(2)/2;    
     end
-    
+    % 形成矩阵然后求A
     origin_matrix = [origin(1),origin(3),1; origin(1),origin(4),1; origin(2),origin(3),1; origin(2),origin(4),1]';
     map_matrix = [map(1),map(3),1; map(1),map(4),1; map(2),map(3),1; map(2),map(4),1]';
-    transform = origin_matrix * pinv(map_matrix);
+    transform = map_matrix * pinv(origin_matrix);
 end
 if length(origin) == 6
+    %同样的，这里是6个点（梯形）的情况
     for i = 1:2
         origin(i) = origin(i) - shape_A(1)/2;
         map(i) = map(i) - shape_swl(1)/2;
--- codes/main.m
+++ codes/main.m
@@ -1,5 +1,5 @@
 clc;clear;
-A = imread('D:\Documents\课程介绍\数据可视化\TeamProject1\zc.jpg');
+A = imread('../test_images/zc.jpg');
 A = double(A);
 image(A)
 left_eyeA = [384, 400, 568, 620];
@@ -7,7 +7,7 @@ right_eyeA = [366, 390, 665, 715];
 noseA = [380,445,630,660,620,680];
 mouthA = [460,490,600,700];
 
-swl = imread('D:\Documents\课程介绍\数据可视化\TeamProject1\swl.jpg');
+swl = imread('../test_images/swl.jpg');
 swl = double(swl);
 image(swl)
 left_eye_swl = [320, 350, 280, 330];
